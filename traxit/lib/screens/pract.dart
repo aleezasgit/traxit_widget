d7a4724750ce305e72bb127d4ad8c022b559f459
aleezasgit <aleezasgit@gmail.com>
2026-02-11 17:18:21 +0500

    feat:'home'

---
+++ traxit/lib/screens/pract.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class screen extends StatelessWidget {
+  const screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
